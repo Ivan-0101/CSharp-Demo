4dd8c8bd632d9737720d18bdc9a31b733cb4a117
Ivan-0101 <136982985+Ivan-0101@users.noreply.github.com>
2025-01-25 18:58:31 +0000

    Console output

--- main.cs
+++ main.cs
@@ -2,6 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Generic message");
+    Console.WriteLine ("Generic message 2");
+    Console.WriteLine ("5+5");
+    Console.WriteLine (5+5);
   }
 }
