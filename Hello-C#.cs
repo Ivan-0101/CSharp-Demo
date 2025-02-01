90e7b6043212961cfcc84bd8636b2207f38d79cb
Ivan-0101 <136982985+Ivan-0101@users.noreply.github.com>
2025-02-01 21:42:58 +0000

    new commands for git

--- Hello-C#.cs
+++ Hello-C#.cs
@@ -5,3 +5,12 @@ class Program {
     Console.WriteLine("Hello, C#");
   }
 }
+
+/*
+git branch - Check which branch you're currently on.
+git checkout branch-name – Switch to the desired branch.
+git checkout -- name-of-the-file-to-discard - Discard changes of a file stopping you from switching between branches.
+git add . – Stage your changes.
+git commit -m "message" – Commit your changes.
+git push origin branch-name – Push to the specific remote branch.
+*/
