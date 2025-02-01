9f57d329d743c244860af710964e2d54e83c49cd
Ivan-0101 <136982985+Ivan-0101@users.noreply.github.com>
2025-02-01 22:01:36 +0000

    more commands

--- Hello-C#.cs
+++ Hello-C#.cs
@@ -10,6 +10,7 @@ class Program {
 git branch - Check which branch you're currently on.
 git checkout branch-name – Switch to the desired branch.
 git checkout -- name-of-the-file-to-discard - Discard changes of a file stopping you from switching between branches.
+git branch -D new-branch-name - Delete a branch.
 git add . – Stage your changes.
 git commit -m "message" – Commit your changes.
 git push origin branch-name – Push to the specific remote branch.
