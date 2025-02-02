45a7d69987cbda502085ecb533297a725ab18e25
Ivan-0101 <136982985+Ivan-0101@users.noreply.github.com>
2025-02-02 19:58:56 +0000

    Adding Problem-1 and some comments

---
+++ Problem-1.cs
@@ -0,0 +1,14 @@
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+
+    int squareSize = int.Parse(Console.ReadLine());
+    
+    for (int i = 0; i < squareSize; i++) {
+      for (int j = 0; j < squareSize; j++){
+        Console.Write("* ");
+      }
+      Console.WriteLine();
+    }
+  }
+}
