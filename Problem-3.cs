c49ecc777ed133770e1c07087ac7dd96c187aae6
Ivan-0101 <136982985+Ivan-0101@users.noreply.github.com>
2025-03-02 19:58:18 +0000

    Removed comments

--- Problem-3.cs
+++ Problem-3.cs
@@ -26,19 +26,3 @@ if (countOfNegativeNumbers % 2 != 0 && countOfNegativeNumbers != 0)
     }
     }
   }
-
-
-
-/*
-git branch - Check which branch you're currently on (Local).
-git branch -r - List all branches (Remote).
-git branch -a - List all branches (Local + Remote).
-git checkout branch-name – Switch to the desired branch.
-git checkout -- name-of-the-file-to-discard - Discard changes of a file stopping you from switching between branches.
-git stash --include-untracked - Save changes to a file (that you don't want to discard) stopping you from switching between branches.
-git branch -D new-branch-name - Delete a branch.
-git fetch --prune - Remove any branches that have been deleted on the remote server.
-git add . - Add all files to the staging area.
-git commit -m "message" – Commit your changes.
-git push origin branch-name – Push to the specific remote branch.
-*/
