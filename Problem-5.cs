5c703ed8638a566e4590a587fe3ee18d1172c1d1
Ivan-0101 <136982985+Ivan-0101@users.noreply.github.com>
2025-05-03 18:42:25 +0000

    Problem-5

---
+++ Problem-5.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Problem5Class {
+  public static void Main (string[] args) {
+
+      List<int> numbers1 = Console.ReadLine()
+          .Split(" ")
+          .Select(int.Parse)
+          .ToList();
+
+      List<int> numbers2 = Console.ReadLine()
+          .Split(" ")
+          .Select(int.Parse)
+          .ToList();
+
+      List<int> resultList = new List<int>();
+
+      int longerList = Math.Max(numbers1.Count, numbers2.Count);
+
+      for (int index = 0; index <= longerList - 1; index++)
+      {
+          if (numbers1.Count - 1 >= index)
+          {
+              resultList.Add(numbers1[index]);
+          }
+          if (numbers2.Count - 1 >= index)
+          {
+              resultList.Add(numbers2[index]);
+          }
+      }
+
+      Console.WriteLine(string.Join(" ", resultList));
+      }
+    }
