24427f5878c7458c99824c37cbfc919bae14566f
Ivan-0101 <136982985+Ivan-0101@users.noreply.github.com>
2025-05-03 18:49:07 +0000

    Problem-6

---
+++ Problem-6.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Problem6Class {
+  public static void Main (string[] args) {
+
+List<int> input = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+for (int i = 0; i <= input.Count - 2; i++)
+{
+    int num1 = input[i];
+    int num2 = input[i + 1];
+
+    if (num1 == num2)
+    {
+        input[i] = num1 + num2;
+        input.RemoveAt(i + 1);
+        i = -1;
+    }
+}
+
+Console.WriteLine(string.Join(" ", input));
+      }
+    }
